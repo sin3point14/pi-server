feb7c1d6af2c8d374e0c14cd9685a7cd41aaf781
sin3point14 <manaschaudhary2000@gmail.com>
2019-08-20 01:18:08 +0530

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
 
@@ -21,10 +21,10 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -66,3 +66,5 @@ sockIO.on('connection', function(socket){
 })
 
 module.exports = app;
+
+//"start": "node --nolazy --inspect-brk=9229 bin/www"
